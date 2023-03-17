7c4321cee6e5c9d225344922f957b4e60d0d8a75
chang <2622310479@qq.com>
2023-03-17 17:45:04 +0800

    feat: edit Welcome component

--- src/pages/welcome/index.vue
+++ src/pages/welcome/index.vue
@@ -1,12 +1,30 @@
 <template>
-  <div color-pink>{{ msg }}</div>
+  <div h-100vh flex="~ col" justify-center items-center>
+    <span text-3xl color-pink>{{ msg }}</span>
+
+    <div flex gap-2 mt-6>
+      <button @click="calc('-')">-</button>
+      <span>{{ count }}</span>
+      <button @click="calc('+')">+</button>
+    </div>
+  </div>
 </template>
 
 <script>
+const { ref } = VueCompositionAPI
 module.exports = {
   setup() {
+    const count = ref(0)
+
+    function calc(type) {
+      if (type === '+') count.value++
+      else if (type === '-') count.value--
+    }
+
     return {
-      msg: 'Hello World!'
+      msg: 'Hello World!',
+      count,
+      calc
     }
   }
 }
