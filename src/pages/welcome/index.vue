0927fb55f1aabc145074ca23244906a48195f433
chang <2622310479@qq.com>
2023-03-19 19:12:25 +0800

    chore: import VueUse package

--- src/pages/welcome/index.vue
+++ src/pages/welcome/index.vue
@@ -2,6 +2,7 @@
   <div h-100vh flex="~ col" justify-center items-center>
     <span text-3xl color-pink>{{ msg }}</span>
 
+    <div>{{ x }}, {{ y }}</div>
     <div flex gap-2 mt-6>
       <button @click="calc('-')">-</button>
       <span>{{ count }}</span>
@@ -12,6 +13,7 @@
 
 <script>
 const { ref } = VueDemi
+const { useMouse } = VueUse
 module.exports = {
   setup() {
     const count = ref(0)
@@ -21,9 +23,14 @@ module.exports = {
       else if (type === '-') count.value--
     }
 
+    // tracks mouse position
+    const { x, y } = useMouse()
+
     return {
       msg: 'Hello World!',
       count,
+      x,
+      y,
       calc
     }
   }
