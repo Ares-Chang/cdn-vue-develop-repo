d599cb910a275eda2101e7f62ad83dc21b540920
chang <2622310479@qq.com>
2023-03-23 13:10:19 +0800

    fefactor: 抽离 welcom 组件

---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<script>
+const { isVue2 } = VueDemi
+const Welcome = '/src/components/Welcome.vue'
+module.exports = {
+  components: {
+    // 此操作为兼容 Vue 2 和 Vue 3 同时使用，常规开发可以选择对应版本加载组件
+    Welcome: isVue2
+      ? httpVueLoader(Welcome)
+      : Vue.defineAsyncComponent(() => loadModule(Welcome, options))
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Welcome></Welcome>
+  </div>
+</template>
