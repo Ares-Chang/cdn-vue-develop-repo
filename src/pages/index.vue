56f6aa09556eb7383694fd8744fe21dd84ba21a9
chang <2622310479@qq.com>
2023-03-25 18:55:30 +0800

    perf:  v2 and v3 get file function

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,18 @@
 <script>
-const { isVue2 } = VueDemi
-const Welcome = '/src/components/Welcome.vue'
+/**
+ * 获取文件
+ * 此操作为兼容 Vue 2 和 Vue 3 同时使用，常规开发可以选择对应版本加载组件
+ * @param {string} url 文件地址
+ */
+function getVueFile(url) {
+  const { isVue2 } = VueDemi
+  return isVue2
+    ? httpVueLoader(url)
+    : Vue.defineAsyncComponent(() => loadModule(url, options))
+}
 module.exports = {
   components: {
-    // 此操作为兼容 Vue 2 和 Vue 3 同时使用，常规开发可以选择对应版本加载组件
-    Welcome: isVue2
-      ? httpVueLoader(Welcome)
-      : Vue.defineAsyncComponent(() => loadModule(Welcome, options))
+    Welcome: getVueFile('/src/components/Welcome.vue')
   }
 }
 </script>
