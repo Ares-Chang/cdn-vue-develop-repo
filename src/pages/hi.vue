ca8229c62cbb4e06848cbe05eacfbfc4e8e03e12
chang <2622310479@qq.com>
2023-03-28 15:51:59 +0800

    perf: move getVueFile function to utils

--- src/pages/hi.vue
+++ src/pages/hi.vue
@@ -1,15 +1,4 @@
 <script>
-/**
- * 获取文件
- * 此操作为兼容 Vue 2 和 Vue 3 同时使用，常规开发可以选择对应版本加载组件
- * @param {string} url 文件地址
- */
-function getVueFile(url) {
-  const { isVue2 } = VueDemi
-  return isVue2
-    ? httpVueLoader(url)
-    : Vue.defineAsyncComponent(() => loadModule(url, options))
-}
 module.exports = {
   components: {
     'the-hello': getVueFile('/src/components/TheHello.vue'),
