404b31f4dc0eca9b94d0d1f68494602cc3155a34
chang <2622310479@qq.com>
2023-03-28 15:55:42 +0800

    perf: optimized getVueFile function

--- src/composables/utils.js
+++ src/composables/utils.js
@@ -2,10 +2,37 @@
  * 获取文件
  * 此操作为兼容 Vue 2 和 Vue 3 同时使用，常规开发可以选择对应版本加载组件
  * @param {string} url 文件地址
+ * @param {object | null} options vue3-sfc-loader 配置项
  */
-function getVueFile(url) {
+function getVueFile(url, options) {
   const { isVue2 } = VueDemi
-  return isVue2
-    ? httpVueLoader(url)
-    : Vue.defineAsyncComponent(() => loadModule(url, options))
+
+  if (isVue2) return httpVueLoader(url)
+
+  return Vue.defineAsyncComponent(() =>
+    loadModule(url, {
+      moduleCache: {
+        vue: Vue
+      },
+      async getFile(url) {
+        const res = await fetch(url)
+        if (!res.ok)
+          throw Object.assign(new Error(res.statusText + ' ' + url), {
+            res
+          })
+        return {
+          getContentData: asBinary =>
+            asBinary ? res.arrayBuffer() : res.text()
+        }
+      },
+      addStyle(textContent) {
+        const style = Object.assign(document.createElement('style'), {
+          textContent
+        })
+        const ref = document.head.getElementsByTagName('style')[0] || null
+        document.head.insertBefore(style, ref)
+      },
+      ...options
+    })
+  )
 }
